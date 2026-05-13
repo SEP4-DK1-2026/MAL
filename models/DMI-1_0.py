5cf764dcd29c7485d631e958e21f707b7ffbed9e
FPC-VIAUC <354412@via.dk>
2026-05-13 13:27:41 +0200

    Add DMI-1_0 model

---
+++ models/DMI-1_0.py
@@ -0,0 +1,49 @@
+from sklearn.pipeline import Pipeline, FunctionTransformer
+from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.multioutput import MultiOutputRegressor
+import pandas as pd
+from pathlib import Path
+from utils import split_time, get_sets
+
+
+def train_model():
+
+    model = MultiOutputRegressor(
+        HistGradientBoostingRegressor(
+            learning_rate=0.05,
+            max_depth=8,
+            max_features=0.6,
+            max_iter=200,
+            max_leaf_nodes=None,
+            min_samples_leaf=33,
+            random_state=42,
+        )
+    )
+
+    dropper_pipeline = Pipeline(
+        [
+            ("NaN_dropper", FunctionTransformer(lambda X: X.dropna())),
+        ]
+    )
+    preprocessor = Pipeline(
+        [
+            ("dropper_pipeline", dropper_pipeline),
+            (
+                "time_spliter",
+                FunctionTransformer(
+                    lambda X: split_time(X, year=False, make_cyclic=True)
+                ),
+            ),
+        ]
+    )
+    model = Pipeline([("preprocessor", preprocessor), ("model", model)])
+
+    raw_data = pd.read_parquet(
+        Path(__file__, "../../data/historical_data_06102.parquet").resolve()
+    )
+    new_data = dropper_pipeline.fit_transform(raw_data)
+    new_data = new_data.sample(n=1000000, random_state=42)
+    X, y = get_sets(new_data, seed=42, do_split=False)
+    model.fit(X, y)
+
+    return model
